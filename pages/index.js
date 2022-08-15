1805e177df902090c4e1ca10f9a420d145ead3a0
Miguel Gargallo <mgargallo16@gmail.com>
2022-08-15 21:37:15 +0200

    1.02

--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,14 @@ import { useRouter } from 'next/router'
 
 export default function Home() {
   const router = useRouter()
-
   return (
     <div className={styles.container}>
       <Head>
         <title>NewsApp - Home</title>
       </Head>
 
-    <h1>Learning NextJS with ITAMAESAN</h1>
-    <Link href='/about'>About</Link>
-
-  <button onClick={() => router.push('/article/2')}>Go to Article 2</button>
-  </div>
+      <h1>Learning NextJS with ITAMAESAN</h1>
+      <Link href='/about'>About</Link>
+    </div>
   )
 }
